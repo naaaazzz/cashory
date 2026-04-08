b1b4dc97cc90e0b5d62d409f9fdf6f8c5fd0522d
nazhoz <chemmalaniyas1145@gmail.com>
2026-04-08 22:18:45 +0530

    initial

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -3,6 +3,7 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
+  clientPrefix: undefined,
   server: {
     DATABASE_URL: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(32),
